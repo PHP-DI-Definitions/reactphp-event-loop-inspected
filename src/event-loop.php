ce29e144d2d68dda40a2e92cf370a1b5f1322711
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-05-14 15:14:27 +0200

    Initial code checkin

---
+++ src/event-loop.php
@@ -0,0 +1,15 @@
+<?php
+
+use React\EventLoop\Factory;
+use React\EventLoop\LoopInterface;
+use WyriHaximus\React\Inspector\EventLoop\InfoProvider;
+use WyriHaximus\React\Inspector\EventLoop\LoopDecorator;
+
+return [
+    LoopInterface::class => function () {
+        return new LoopDecorator(Factory::create());
+    },
+    InfoProvider::class => function (LoopInterface $loop) {
+        return new InfoProvider($loop);
+    },
+];
