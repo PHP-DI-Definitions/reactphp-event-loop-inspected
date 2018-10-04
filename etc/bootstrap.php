92bc885cb4a8ef4e21d56bd256d48dce9216ae4e
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-10-04 20:14:33 +0200

    Run loop one to clear shutdown function from running the loop again

--- etc/bootstrap.php
+++ etc/bootstrap.php
@@ -1,7 +1,9 @@
 <?php
 
+use function EventLoop\getLoop;
 use React\EventLoop\Factory;
 use WyriHaximus\React\Inspector\EventLoop\LoopDecorator;
 use function EventLoop\setLoop;
 
 setLoop(new LoopDecorator(Factory::create()));
+getLoop()->run();
