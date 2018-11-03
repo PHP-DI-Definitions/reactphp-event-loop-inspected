5498631e393f9e6f32f094fc377373b3998fa330
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-11-03 23:26:02 +0100

    QA configured and ran

--- etc/bootstrap.php
+++ etc/bootstrap.php
@@ -1,8 +1,8 @@
-<?php
+<?php declare(strict_types=1);
 
-use function EventLoop\getLoop;
 use React\EventLoop\Factory;
 use WyriHaximus\React\Inspector\EventLoop\LoopDecorator;
+use function EventLoop\getLoop;
 use function EventLoop\setLoop;
 
 setLoop(new LoopDecorator(Factory::create()));
--- etc/di/event-loop.php
+++ etc/di/event-loop.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 use React\EventLoop\LoopInterface;
 use function EventLoop\getLoop;
