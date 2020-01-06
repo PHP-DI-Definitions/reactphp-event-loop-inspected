9592bb1e879ded9e32edb83af9db978d5d8d92bc
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-01-06 16:57:13 +0100

    Supply metrics stream

--- etc/di/event-loop.php
+++ etc/di/event-loop.php
@@ -1,10 +1,24 @@
 <?php declare(strict_types=1);
 
+use ReactInspector\EventLoop\LoopCollector;
+use ReactInspector\MemoryUsage\MemoryUsageCollector;
+use ReactInspector\Metrics;
+use ReactInspector\MetricsStreamInterface;
+use ReactInspector\Stream\IOCollector;
 use function EventLoop\getLoop;
 use React\EventLoop\LoopInterface;
 
 return [
     LoopInterface::class => function () {
         return getLoop();
+    },
+    MetricsStreamInterface::class => function (LoopInterface $loop) {
+        return new Metrics(
+            $loop,
+            1,
+            new LoopCollector($loop),
+            new MemoryUsageCollector($loop),
+            new IOCollector()
+        );
     },
 ];
